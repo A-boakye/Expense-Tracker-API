cd6595d01104bfb4314e08eb33a0f7c54afb902d
A-boakye <boakyeabigail644@gmail.com>
2026-07-09 01:28:57 +0000

    create expenses table

---
+++ src/expenses/models.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from datetime import date, datetime,timezone
+from decimal import Decimal
+from ..db.base import Base
+
+from sqlalchemy import Date, DateTime, ForeignKey, Numeric, String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+if TYPE_CHECKING:
+    from ..users.models import User
+    
+
+
+class Expense(Base):
+    __tablename__ = "expenses"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    title: Mapped[str] = mapped_column(String(100))
+
+    amount: Mapped[Decimal] = mapped_column(
+        Numeric(10, 2)
+    )
+
+    category: Mapped[str] = mapped_column(String(50))
+
+    description: Mapped[str | None] = mapped_column(
+        Text,
+        nullable=True
+    )
+
+    date: Mapped[datetime] = mapped_column(Date)
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id")
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+       DateTime(timezone=True), default=datetime.now()
+    )
+
+    user: Mapped["User"] = relationship(
+        back_populates="expenses"
+    )
