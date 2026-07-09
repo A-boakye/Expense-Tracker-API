e7ca63401830e7c1e019a2a524adf4bb18cf4ae1
A-boakye <boakyeabigail644@gmail.com>
2026-07-09 02:19:41 +0000

    create expense schema

---
+++ src/expenses/schema.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+class ExpenseCreate(BaseModel):
+    title: str
+    amount: float
+    category: str | None = None
+    description: str | None = None
+
+class ExpenseRead(BaseModel):
+    id: int
+    title: str
+    amount: float
+    category: str |  None
+    decription: str | None 
+    created_at: datetime
+    user_id: int
+
+    model_config = {"from_attributes": True}
+
+class ExpenseUpdate(BaseModel):
+    pass
+
